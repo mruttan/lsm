f73cbec545ae4d29bae2c694e336aca79c1a7bca
matt <you@example.com>
2019-05-15 18:11:36 -0400

    fixed google map implimentation

--- imports/ui/ContactMap.js
+++ imports/ui/ContactMap.js
@@ -1,18 +1,59 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+// import { withRouter } from 'react-router-dom';
 
 export class ContactMap extends React.Component {
+
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBW4XXdeFMVbZqMaWR8n6h-AGCSDrbopR0&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    var latlng = {
+      lat: 44.038234,
+      lng: -79.451330
+    }
+    
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: latlng,
+      zoom: 15
+    });
+
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map
+    });
+  }
+  
+
   render() {
     return (
-      <div>
-        <h2>Location</h2>
-          <h5>16610 Bayview Ave</h5>
-          <h5>Just south of Mulock & Bayview</h5>
-          <div id="map"></div>
-          <hr className="d-sm-none"/>
-      </div>
+      <main>
+        <div>
+          <h2>Location</h2>
+            <h5>16610 Bayview Ave</h5>
+            <h5>Just south of Mulock & Bayview</h5>
+            <div id="map"></div>
+            <hr className="d-sm-none"/>
+
+        </div>
+
+      </main>
     );
   }
 }
 
-export default withRouter(ContactMap);
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
+}
+
+export default ContactMap;
