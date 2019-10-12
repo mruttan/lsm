cd0cd5985e560186b828f2a5de168d74a6d24ad0
Matt Ruttan <matthew.ruttan5@gmail.com>
2019-10-12 18:04:20 -0400

    development branch push to test auto deploy on staging app

--- imports/ui/SiteContent.js
+++ imports/ui/SiteContent.js
@@ -35,7 +35,7 @@ export class SiteContent extends React.Component {
           <section className="col-12">
 
             <div className="col mx-auto">
-              <h1>heroku test</h1>
+              <h1>heroku test - development app</h1>
               <p>
                   At Lion’s Share our goal is to help you increase your brand's visibility, while generating the highest return on your investment.  
                   Our friendly experienced team has access to thousands of quality promotional products.  We will take the time to define your 
