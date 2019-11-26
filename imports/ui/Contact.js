156e5d098464b984150cd63babaa901a9c81c8cd
Matt Ruttan <matthew.ruttan5@gmail.com>
2019-11-26 02:16:46 -0500

    layouts done - just need to fill in pictures/text blurbs/lshockey page & link highlighting experience of company

--- imports/ui/Contact.js
+++ imports/ui/Contact.js
@@ -19,6 +19,53 @@ export class Contact extends React.Component {
     return (
       <div>
         <SiteHeader title="LSM" />
+
+        <section class="page-section cta">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-12 mx-auto">
+                <div class="cta-inner text-center rounded">
+                  {/* <div class="row">
+                    <div class="col-xl-12 text-center">
+                      <h2 class="section-heading text-uppercase">Contact</h2>
+                      <h3 class="section-subheading text-muted">
+                        Lorem ipsum dolor sit amet consectetur.
+                      </h3>
+                    </div>
+                  </div> */}
+
+                  <div className="row">
+                    <div className="col-sm-3 mt-2">
+                      <h3>Contact</h3>
+                      <ul className="nav nav-pills flex-column">
+                        <li className="nav-item">
+                          <a className="nav-link disabled" href="#">(905) 555-5555</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">email@example.com</a>
+                        </li>
+                      </ul>
+                      <hr/>
+                      <h3>Hours</h3>
+                      <ul className="nav nav-pills flex-column">
+                        <li className="nav-item">
+                          <a className="nav-link disabled">Mon-Fri</a>
+                          <a className="nav-link disabled" href="#">9am-5pm</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-sm-9">
+                      <ContactMap/>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+{/* 
         <div className="container content-container page-section">
           <div className="row">
             <section className="col-12">
@@ -49,19 +96,12 @@ export class Contact extends React.Component {
                       <ContactMap/>
                     </div>
                   </div>
-
-
-
-
                 </div>
               </div>
             </section>
           </div>
+        </div> */}
 
-
-
-
-        </div>
         <SiteFooter/>
       </div>
     );
--- imports/ui/SiteFooter.js
+++ imports/ui/SiteFooter.js
@@ -4,84 +4,20 @@ import { Link, withRouter } from 'react-router-dom';
 class SiteFooter extends React.Component {
   render() {
     return (
-      // <div className="container">
-
-      //   <div className="text-center site-footer">
-      //     <div className="container-fluid">
-      //       <div className="row">
-      //         <div className="col-sm">
-      //           <Link to="/">
-      //             <h5 className="footer-link">Home</h5>
-      //           </Link>
-      //         </div>
-      //         <div className="col-sm">
-      //           <Link to="/about">
-      //             <h5 className="footer-link">About Us</h5>
-      //           </Link>
-      //         </div>
-      //         <div className="col-sm">
-      //           <Link to="/services">
-      //             <h5 className="footer-link">Services</h5>
-      //           </Link>
-      //         </div>
-      //         <div className="col-sm">
-      //           <Link to="/products">
-      //             <h5 className="footer-link">Products</h5>
-      //           </Link>
-      //         </div>
-      //         <div className="col-sm">
-      //           <Link to="/contact">
-      //             <h5 className="footer-link">Contact</h5>
-      //           </Link>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-
-      // </div>
-
-      <div className="mt-1 pt-5 pb-3 footer">
+      <div className="pt-1 footer">
         <div className="container">
           <div className="row">
-            <div className="col my-auto">
-              <Link to="/" className="footer-link">
-                <h4>Home</h4>
-              </Link>
-            </div>
-            <div className="col my-auto">
-              <Link to="/about" className="footer-link">
-                <h4>About Us</h4>
-              </Link>
-            </div>
-            <div className="col my-auto">
-              <Link to="/products" className="footer-link">
-                <h4>Products</h4>
-              </Link>
-            </div>
-            <div className="col my-auto">
-              <Link to="/contact" className="footer-link">
-                <h4>Contact</h4>
-              </Link>
-            </div>
-            {/* <div className="col-7 links">
-              <h4 className="mt-lg-0 mt-sm-3">Links</h4>
-              <ul className="m-0 p-0">
-                <li>- <a href="#">Lorem ipsum</a></li>
-                <li>- <a href="#">Nam mauris velit</a></li>
-                <li>- <a href="#">Etiam vitae mauris</a></li>
-                <li>- <a href="#">Fusce scelerisque</a></li>
-                <li>- <a href="#">Sed faucibus</a></li>
-                <li>- <a href="#">Mauris efficitur nulla</a></li>
-              </ul>
-            </div> */}
             <div className="col location text-center">
-              <h4 className="mt-4">Location</h4>
+              <h4 className="mt-5">Location</h4>
               <p>16610, Bayview Ave, Newmarket</p>
+            </div>
+            <div className="col location text-center">
+              <h4 className="mt-5">Contact</h4>
               <p className="mb-0"><i className="fa fa-phone mr-3"></i>(905) 555-5555</p>
               <p><i className="fa fa-envelope-o mr-3"></i>email@example.com</p>
             </div>
           </div>
-          <div className="row mt-5">
+          <div className="row mt-2">
             <div className="col-5 copyright">
               <p className=""><small className="text-white-50">© 2019. All Rights Reserved.</small></p>
             </div>
